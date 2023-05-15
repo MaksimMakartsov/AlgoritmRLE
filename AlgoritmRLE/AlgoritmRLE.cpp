47cfcd89b72a3fc4df50974c740135bf574284d3
MaksimMakartsov <Maksim.Makartsov.Web@gmail.com>
2023-05-15 12:57:42 +0300

    Добавьте файлы проекта.

---
+++ AlgoritmRLE/AlgoritmRLE.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+std::string compressRLE(std::string str) {
+    std::string compressed = "";
+    int count = 1;
+    char current = str[0];
+    std::cout << "Original length: " << str.size() << std::endl;
+
+    for (int i = 1; i <= str.size(); i++) {
+        if (str[i] != current) {
+            compressed += std::to_string(count) + current;
+            count = 1;
+            current = str[i];
+        }
+        else {
+            count++;
+        }
+    }
+
+    return compressed;
+}
+
+int main() {
+    std::string data;
+
+    std::ifstream fin("input.txt");
+    fin >> data;
+
+    std::string compressed = compressRLE(data);
+    std::cout << "Compressed: " << compressed << std::endl;
+    std::cout << "Original length: " << compressed.size() << std::endl;
+    fin.close();
+    return 0;
+}
